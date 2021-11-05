9c1df0274bebc6b94884fafd21264a190acfc680
KimWanki <seoulkwk95@gmail.com>
2021-11-05 21:32:36 +0900

    feat: user type binding

---
+++ ExampleProject/MVVM_UIKit/MVVM_UIKit/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  MVVM_UIKit
+//
+//  Created by WANKI KIM on 2021/11/05.
+//
+
+import Foundation
+
+struct User {
+    var name: Observable<String>
+}
---
+++ ExampleProject/MVVM_UIKit/MVVM_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_UIKit
+//
+//  Created by WANKI KIM on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
